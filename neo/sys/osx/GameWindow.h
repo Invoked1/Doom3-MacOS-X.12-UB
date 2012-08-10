6a32228c79ce97cc32145cd2ad6a16bdddc3f949
Matthias Maiershofer <matthias.maiershofer@gmail.com>
2012-08-10 21:17:43 +0200

    - remove the ID_ALLOW_D3XP stuff - only show the case-sensitive FS warnings in _DEBUG mode - build d3xp

---
+++ neo/sys/osx/GameWindow.h
@@ -0,0 +1,13 @@
+//
+//  GameWindow.h
+//  Doom3
+//
+//  Created by Matthias Maiershofer on 07/08/2012.
+//  Copyright (c) 2012 id Software LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GameWindow : NSWindow
+
+@end
---
+++ neo/sys/osx/GameWindow.mm
@@ -0,0 +1,13 @@
+//
+//  GameWindow.m
+//  Doom3
+//
+//  Created by Matthias Maiershofer on 07/08/2012.
+//  Copyright (c) 2012 id Software LLC. All rights reserved.
+//
+
+#import "GameWindow.h"
+
+@implementation GameWindow
+
+@end
